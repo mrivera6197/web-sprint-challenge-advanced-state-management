398313bd343f75270aacd210c2a30a28a8f6232e
Mali Rivera <mali@Malis-MacBook-Pro.local>
2020-12-11 13:16:32 -0500

    test commit

--- client/src/index.js
+++ client/src/index.js
@@ -24,3 +24,5 @@ ReactDOM.render(
 //1. Add in all necessary components and libary methods.
 //2. Create a store that includes thunk middleware support.
 //3. Wrap the App component in a react-redux Provider element.
+
+//test comment
